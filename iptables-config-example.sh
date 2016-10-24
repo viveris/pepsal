512c27cab63ec5bbf061cfe338bd26b222a9d79c
Joaquin Muguerza <jmuguerza@toulouse.viveris.fr>
2016-10-24 10:14:01 +0200

    Added Debian packaging

--- iptables-config-example.sh
+++ iptables-config-example.sh
@@ -4,30 +4,11 @@ echo "8192 2100000 8400000" >/proc/sys/net/ipv4/tcp_mem
 echo "8192 2100000 8400000" >/proc/sys/net/ipv4/tcp_rmem
 echo "8192 2100000 8400000" >/proc/sys/net/ipv4/tcp_wmem
 
-SAT_RECV="192.168.200.0/16"
-NQ=9
-OUT_IFACE="wlan0"
 CLIENTS_IFACE="eth0"
 
+ip rule add fwmark 1 lookup 100
+ip route add local 0.0.0.0/0 dev lo table 100
+
+# Careful, as this will erase any other preexisting entries
 iptables -t mangle -F
-iptables -t nat -F
-
-iptables -t nat -F TCP_OPTIMIZATION
-iptables -t mangle -F TCP_OPTIMIZATION
-
-#
-/sbin/iptables -I PREROUTING -t mangle -p tcp --syn -j TCP_OPTIMIZATION
-/sbin/iptables -I PREROUTING -t nat -p tcp --syn -j TCP_OPTIMIZATION
-#/sbin/iptables -I POSTROUTING -t nat -p tcp --syn -j TCP_OPTIMIZATION
-#/sbin/iptables -I POSTROUTING -t nat -s 192.168.200.2 -j MASQUERADE
-
-iptables -t mangle -I TCP_OPTIMIZATION -i eth0 -s 192.168.200.0/24 -p tcp -j NFQUEUE --queue-num=9
-iptables -t nat -A POSTROUTING -s $SAT_RECV  -o $OUT_IFACE -j MASQUERADE
-iptables -t nat -I TCP_OPTIMIZATION -i eth0 -s 192.168.200.0/24 -p tcp -j REDIRECT --to-port 6009
-
-#iptables -A PREROUTING -t nat -p tcp
-#iptables -A PREROUTING -t mangle -p tcp -flags syn
-
-#iptables -t mangle -A PREROUTING -i $CLIENTS_IFACE -p tcp --syn -j NFQUEUE --queue-num=$NQ
-#iptables -t nat -A POSTROUTING -s $SAT_RECV  -o $OUT_IFACE -j MASQUERADE
-#iptables -t nat -A PREROUTING -s $SAT_RECV -p tcp -j REDIRECT --to-port 6001
+iptables -t mangle -A PREROUTING -i $CLIENTS_IFACE -p tcp -j TPROXY --on-port 5000 --tproxy-mark 1
